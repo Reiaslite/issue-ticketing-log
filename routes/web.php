b337a2516506895a7bbbeea939bc3391883c8c06
Reiaslite <raidearaujo06@gmail.com>
2026-05-23 14:08:19 +0700

    feats(routes): add employee routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,12 @@ Route::view('/login', 'auth.login')->name('login');
 Route::view('/', 'welcome')->name('dashboard');
 
 /**
+ * Employee routes
+ */
+Route::view('/employees', 'pages.employees.index')->name('employees.index');
+Route::view('/employees/create', 'pages.employees.create')->name('employees.create');
+
+/**
  * Ticket routes
  */
 Route::view('/tickets', 'pages.tickets.index')->name('tickets.index');
